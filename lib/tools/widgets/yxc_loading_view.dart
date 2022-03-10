64e6232110605b963030ff7f053479c099e6ce47
guoguangtao <Jude_guo@163.com>
2022-03-10 19:26:31 +0800

    网络加载视图修改

--- lib/tools/widgets/yxc_loading_view.dart
+++ lib/tools/widgets/yxc_loading_view.dart
@@ -16,27 +16,52 @@ class YXCLoadingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       type: MaterialType.transparency,
-      child: Center(
-        child: Container(
-          width: 80,
-          height: 80,
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: double.maxFinite,
+            margin: const EdgeInsets.only(left: 32.5, right: 32.5),
+            padding: const EdgeInsets.only(top: 36.0, bottom: 36.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.transparent,
+                  width: 36.0,
+                  height: 36.0,
+                  child: const CircularProgressIndicator(
+                    color: Color(0xFF1979FF),
+                    strokeWidth: 3.0,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                const Text(
+                  "正在请求网络...",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16.0,
+                      color: Color(0xFF17181A)),
+                ),
+              ],
+            ),
           ),
-          child: const CircularProgressIndicator(
-            color: Colors.grey,
-          ),
-        ),
+        ],
       ),
     );
   }
 
   static show(BuildContext context) {
-    showDialog(context: context, barrierDismissible: false, builder: (context) {
-      return _loadingView;
-    });
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return _loadingView;
+        });
   }
 
   static hidden(context) {
